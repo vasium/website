85f49559414e506bc52da4754b24570a86bda496
evzonic <evzonic@gmail.com>
2021-06-08 22:32:05 +0200

    first commit

---
+++ src/Header.js
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import * as Unicons from "@iconscout/react-unicons";
+
+const Header = () => {
+  const [navMenu, setnavMenu] = useState(null);
+  const [navToggle, setnavToggle] = useState(null);
+  const [navClose, setnavClose] = useState(null);
+  const [navLink, setnavLink] = useState(null);
+
+  useEffect(() => {
+    setnavMenu(document.getElementById("nav-menu"));
+    setnavToggle(document.getElementById("nav-toggle"));
+    setnavClose(document.getElementById("nav-close"));
+    setnavLink(document.querySelectorAll(".nav__link"));
+  }, []);
+
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
+    });
+  }
+
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
+
+  const linkAction = (e) => {
+    e.preventDefault();
+    navMenu.classList.remove("show-menu");
+  };
+
+  if (navLink) {
+    navLink.forEach((element) => {
+      element.addEventListener("click", linkAction);
+    });
+  }
+
+  const [textField, setTextField] = useState(null);
+  const currentElementIdContent = " evzonic ";
+  useEffect(() => {
+    let i = 0;
+    setInterval(() => {
+      if (i < currentElementIdContent.length) {
+        const text = currentElementIdContent.slice(0, i++);
+        text !== currentElementIdContent && setTextField(text);
+      }
+    }, 100);
+  }, []);
+
+  return (
+    <header className="header" id="header">
+      <nav className="nav container">
+        <a href="/" className="nav__logo">
+          <div>
+            <span className="typeing">
+              {textField}
+              <span className="blinker"></span>
+            </span>
+          </div>
+        </a>
+        <div className="nav__menu" id="nav-menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                <Unicons.UilEstate className="nav__icon" />
+                Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                <Unicons.UilUser className="nav__icon" />
+                About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                <Unicons.UilFileAlt className="nav__icon" />
+                Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                <Unicons.UilBriefcaseAlt className="nav__icon" />
+                Sevices
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                <Unicons.UilScenery className="nav__icon" />
+                Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                <Unicons.UilMessage className="nav__icon" />
+                Contact
+              </a>
+            </li>
+          </ul>
+          <Unicons.UilTimes className="nav__close" id="nav-close" />
+        </div>
+        <div className="nav__btns">
+          <div className="nav__toggle" id="nav-toggle">
+            <Unicons.UilApps />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,86 @@
+import * as Unicons from "@iconscout/react-unicons";
+import prefil from "./images/perfil.png";
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <div className="home__social">
+            <a
+              href="https://www.linkedin.com/"
+              target="blank"
+              className="home__social-icon"
+            >
+              <Unicons.UilLinkedin className="nav__icon" />
+            </a>
+            <a
+              href="https://dribbble.com/"
+              target="blank"
+              className="home__social-icon"
+            >
+              <Unicons.UilDribbble className="nav__icon" />
+            </a>
+            <a
+              href="https://github.com/"
+              target="blank"
+              className="home__social-icon"
+            >
+              <Unicons.UilGithub className="nav__icon" />
+            </a>
+          </div>
+
+          <div className="home__img">
+            <svg
+              className="home__blob"
+              viewBox="0 0 200 187"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
+              <mask id="mask0" mask-type="alpha">
+                <path
+                  d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
+        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
+        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
+                />
+              </mask>
+              <g mask="url(#mask0)">
+                <path
+                  d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
+        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
+        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
+        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
+                />
+                <image className="home__blob-img" xlinkHref={prefil} />
+              </g>
+            </svg>
+          </div>
+          <div className="home__data">
+            <h1 className="home__title">Hi, I am Vasilis</h1>
+            <h3 className="home__subtitle">
+              Web <span className="dot"></span> XR <span className="dot"></span>{" "}
+              iOS Developer
+            </h3>
+            <p className="home__description">
+              High level experience in Web development, XR development &amp; iOS
+              development.
+            </p>
+            <a href="#contact" className="button button--flex">
+              Contact Me <Unicons.UilMessage className="button__icon uil" />
+            </a>
+          </div>
+        </div>
+
+        <div className="home__scroll">
+          <a href="@about" className="home__scroll-button button--flex">
+            <Unicons.UilMouseAlt className="home__scroll-mouse uil" />
+            <span className="home__scroll-name">Scroll down</span>
+            <Unicons.UilArrowDown className="home__scroll-arrow uil" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
