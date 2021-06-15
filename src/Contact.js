0426d51509efdd939bdde95c5cf1b7715687bbbe
evzonic <evzonic@gmail.com>
2021-06-15 11:17:08 +0200

    corrected the  netlify form submision feature

--- src/Contact.js
+++ src/Contact.js
@@ -86,6 +86,31 @@ const Contact = () => {
             </button>
           </div>
         </form>
+
+        <div>
+          <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+          ,
+        </div>
       </div>
     </section>
   );
